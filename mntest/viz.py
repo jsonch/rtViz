ee5034892688b67f5b2d23d306116949938ac9a1
John S <jsonch@princeton.edu>
2021-03-25 10:48:59 -0400

    blitting

--- mntest/viz.py
+++ mntest/viz.py
@@ -14,13 +14,24 @@ import numpy as np
 from matplotlib import animation
 import matplotlib.patches as mpatch
 
+# use real measurements or random data
+testMode = False
+
 client = "10.0.0.1"
 attacker = "10.0.0.3"
-interval = 1
+interval = .2 # how often to redraw the plot
+# how many b/s to observe before drawing the "attack!" box
+attack_threshold = 8**8 # 800 Mb/s
+
+# arrow scaling parameters
+min_arrow_width = 1.0
+max_arrow_width = 5.0
+min_flow_rate = 0
+max_flow_rate = 10**9 # 1 Gb/s
+
 
 def main():
   measQ = Queue()
-
   mthread = Thread(target = measLoop, args = (client, attacker, measQ, interval))
   mthread.daemon = True
   mthread.start()
@@ -39,6 +50,10 @@ def measLoop(clientIp, attackIp, measQ, interval = 1):
     meas_delta = calcDelta(meas, new_meas)
     meas = new_meas
     time.sleep(interval)
+    # generate random data if testmode
+    if (testMode):
+      meas_delta["client_bytes"] = 1000000
+      meas_delta["attack_bytes"] = 100000000
     measQ.put(meas_delta)
 
 def measFlows(clientIp, attackIp):
@@ -70,11 +85,9 @@ def calcDelta(prev, cur):
 
 
 ### Plotting 
-# how many b/s to observe before drawing the "attack!" box
-attack_threshold = 400000000 # 400 Mb/s
 
-num_frames_to_record = 80
-record_mode = False
+# num_frames_to_record = 80
+# record_mode = False
 
 next_hop = {
   "Attacker":"Switch",
@@ -123,42 +136,120 @@ def startTopoPlot():
   G.add_edge('Attacker', 'Switch', color = 'red', flow='bad')
   G.add_edge('Switch', 'Server', rad = .1, color = 'blue', flow='good')
   G.add_edge('Switch', 'Server', rad = .4, color = 'red', flow='bad')
-
   return G, ax, fig
 
 
 def runTopoPlot(measQ, G, ax, fig):
-  global num_frames_to_record, record_mode
-  if (not record_mode):
-    num_frames_to_record = 4
-  # run the plot. update in real time, saving the first K frames
-  ani = animation.FuncAnimation(fig, topoQuiverUpdate, frames=num_frames_to_record, fargs=(measQ, G, ax))
-  if (record_mode):
-    ani.save('offlineTopoPlot.gif', writer='imagemagick')
-  plt.show()
+  # get the static background.
+  fig.canvas.draw()
+  bg = fig.canvas.copy_from_bbox(ax.bbox)
+  plt.show(False)
+
+  # plot-update loop.
+  fnum = 0
+  while (True):
+    curtime, rates = get_time_and_rates(measQ)
+    # reset background. 
+    fig.canvas.restore_region(bg)
+    plot_edges(bg, fig, ax, G, rates, fnum)
+    plot_nodes(bg, fig, ax, G)
+
+    # 400 MB/s
+    if (rates[frozenset(("Attacker", "Server"))] > attack_threshold):
+      plot_alert(bg, fig, ax)
 
 
-def direction_of(cur_pos, next_pos): 
-  direction = [next_pos[i] - cur_pos[i] for i in range(len(cur_pos))]
-  return direction
+    # fill in the axes rectangle
+    # fig.canvas.draw()
+    fig.canvas.blit(ax.bbox)
+    fnum += 1
+  return 
 
-def interp_vec_of(cur_pos, next_pos, offset, n):
-  total_delta = np.array(direction_of(cur_pos, next_pos))
-  vec = []
-  delta = total_delta/float(n)
-  for i in range(n):      
-    vec.append(cur_pos + (i+offset)*delta)
-  return vec
-      
-# arrow scaling parameters
-min_arrow_width = 1.0
-max_arrow_width = 5.0
-min_flow_rate = 0
-max_flow_rate = 10**9 # 1 Gb/s
-def bps_to_arrow_width (rate):
-  pt_width = ((float(rate) / max_flow_rate) * (max_arrow_width - min_arrow_width)) + min_arrow_width
-  pt_width = (pt_width) * .005
-  return pt_width
+
+last_plottime = None
+def get_time_and_rates(measQ):
+  global last_plottime
+  data_in = measQ.get()
+  if (last_plottime == None):
+    curtime = data_in["ts"]
+  else:
+    curtime = last_plottime+data_in["ts"]
+  last_plottime = curtime
+
+  seconds_elapsed = data_in["ts"]/1000.0
+  cli_to_switch = (data_in["client_bytes"]*8.0)/seconds_elapsed
+  cli_to_server = cli_to_switch
+  atk_to_switch = (data_in["attack_bytes"]*8.0)/seconds_elapsed
+  atk_to_server = atk_to_switch
+  print ("curtime: %s cli_to_switch: %s, atk_to_switch: %s, cli_to_server: %s, atk_to_server: %s"%
+  (curtime, cli_to_switch, atk_to_switch, cli_to_server, atk_to_server))
+  rates = {
+  frozenset(("Client", "Switch")):cli_to_switch,
+  frozenset(("Attacker", "Switch")):atk_to_switch,
+  frozenset(("Client", "Server")):cli_to_server,
+  frozenset(("Attacker", "Server")):atk_to_server
+  }
+  return curtime, rates
+
+
+def plot_nodes(bg, fig, ax, G):
+  artists = []
+  for node in G.nodes(data=True):
+    a = nx.draw_networkx_nodes(
+      G, 
+      node_pos, 
+      [node[0]], 
+      # node_color=node[1]['color'], 
+      linewidths=5,
+      edgecolors=node[1]['color'],
+      node_color="white",
+      ax = ax, 
+      node_size=2500,
+      node_shape = "s"
+    )
+    artists.append(a)
+  ldict = nx.draw_networkx_labels(G, pos=node_pos, ax=ax)
+  for pt in artists+list(ldict.values()):
+      ax.draw_artist(pt)
+
+
+def plot_alert(bg, fig, ax):
+  rectangles = {'Attack!' : mpatch.Rectangle((.25, -.5), .5, .25, linewidth=1, edgecolor='r', color='r')}
+  artists = []
+  for r in rectangles:
+    a = ax.add_artist(rectangles[r])
+    rx, ry = rectangles[r].get_xy()
+    cx = rx + rectangles[r].get_width()/2.0
+    cy = ry + rectangles[r].get_height()/2.0
+
+    an = ax.annotate(r, (cx, cy), color='w', weight='bold', 
+          fontsize=12, ha='center', va='center')
+    artists.append(a)
+    artists.append(an)
+
+  for pt in artists:
+    ax.draw_artist(pt)
+
+
+
+
+def plot_edges(bg, fig, ax, G, rates, framenum):
+  # draw each edge individually. 
+  all_pts = []
+  for edge in G.edges(data=True):
+    edge_key = frozenset((edge[0], edge[1]))
+    if (edge[0]=='Switch' and edge[1] == 'Server'):
+      if (edge[2]['flow']=='bad'):
+        edge_key = frozenset(("Attacker", "Server"))
+      else:
+        edge_key = frozenset(("Client", "Server"))
+    rate = rates[edge_key]
+    pts = plot_edge(ax, edge, rate, (framenum%2*.5))
+    all_pts += pts
+  # draw new points
+  for pt in all_pts:
+    ax.draw_artist(pt)
+
 
 def plot_edge(ax, edge, rate, frame_offset):
   if (rate == 0):
@@ -181,83 +272,33 @@ def plot_edge(ax, edge, rate, frame_offset):
     num_pts = 5
   pt_width = bps_to_arrow_width(rate)
   vec = interp_vec_of(cur_pos, next_pos, flow_offset[flowtype] + frame_offset, num_pts)
+  pts = []
   for pt in vec:
-    ax.quiver(pt[0], pt[1], direction[0], direction[1], 
+    pts.append(ax.quiver(pt[0], pt[1], direction[0], direction[1], 
       color = flow_color[flowtype], 
       scale =  10,
       width = pt_width
-    )
+    ))
+  return pts
 
-def draw_alert(ax):
-  rectangles = {'Attack!' : mpatch.Rectangle((.25, -.5), .5, .25, linewidth=1, edgecolor='r', color='r')}
+def direction_of(cur_pos, next_pos): 
+  direction = [next_pos[i] - cur_pos[i] for i in range(len(cur_pos))]
+  return direction
 
-  for r in rectangles:
-    ax.add_artist(rectangles[r])
-    rx, ry = rectangles[r].get_xy()
-    cx = rx + rectangles[r].get_width()/2.0
-    cy = ry + rectangles[r].get_height()/2.0
+def interp_vec_of(cur_pos, next_pos, offset, n):
+  total_delta = np.array(direction_of(cur_pos, next_pos))
+  vec = []
+  delta = total_delta/float(n)
+  for i in range(n):      
+    vec.append(cur_pos + (i+offset)*delta)
+  return vec
+      
+def bps_to_arrow_width (rate):
+  pt_width = ((float(rate) / max_flow_rate) * (max_arrow_width - min_arrow_width)) + min_arrow_width
+  pt_width = (pt_width) * .005
+  return pt_width
 
-    ax.annotate(r, (cx, cy), color='w', weight='bold', 
-          fontsize=12, ha='center', va='center')
 
-
-
-last_plottime = None
-def topoQuiverUpdate(num, measQ, G, ax):
-  # plot refresh function. Can be much more efficient. Oh well.
-  global last_plottime
-  data_in = measQ.get()
-  if (last_plottime == None):
-    curtime = data_in["ts"]
-  else:
-    curtime = last_plottime+data_in["ts"]
-  last_plottime = curtime
-
-  cli_to_switch = (data_in["client_bytes"]*8.0)
-  cli_to_server = cli_to_switch
-  atk_to_switch = (data_in["attack_bytes"]*8.0)
-  atk_to_server = atk_to_switch
-  print ("curtime: %s cli_to_switch: %s, atk_to_switch: %s, cli_to_server: %s, atk_to_server: %s"%
-  (curtime, cli_to_switch, atk_to_switch, cli_to_server, atk_to_server))
-  rates = {
-  frozenset(("Client", "Switch")):cli_to_switch,
-  frozenset(("Attacker", "Switch")):atk_to_switch,
-  frozenset(("Client", "Server")):cli_to_server,
-  frozenset(("Attacker", "Server")):atk_to_server
-  }
-  ax.clear()
-  ax.set_xlim((-1, 1))
-  ax.set_ylim((-1, 1))
-  for node in G.nodes(data=True):
-    nx.draw_networkx_nodes(
-      G, 
-      node_pos, 
-      [node[0]], 
-      # node_color=node[1]['color'], 
-      linewidths=5,
-      edgecolors=node[1]['color'],
-      node_color="white",
-      ax = ax, 
-      node_size=2500,
-      node_shape = "s"
-    )
-  nx.draw_networkx_labels(G, pos=node_pos, ax=ax)
-  # draw each edge individually. 
-  for edge in G.edges(data=True):
-    edge_key = frozenset((edge[0], edge[1]))
-    if (edge[0]=='Switch' and edge[1] == 'Server'):
-      if (edge[2]['flow']=='bad'):
-        edge_key = frozenset(("Attacker", "Server"))
-      else:
-        edge_key = frozenset(("Client", "Server"))
-    rate = rates[edge_key]
-    plot_edge(ax, edge, rate, (num%2*.5))
-
-  # 400 MB/s
-  if (atk_to_switch > 400000000):
-    draw_alert(ax)
-
-  ax.set_title("Time {0}".format(curtime))
 
 
 
